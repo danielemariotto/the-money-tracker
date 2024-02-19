5f3bd1f4dec05773d3cab0cf9e0bc01a483b1101
dan <daniele.mariotto@gmail.com>
2024-02-19 18:09:35 +0100

    Aggiunti componenti di inserimento transazione e dettaglio transazione

---
+++ src/components/insertTransaction.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    Import:<input type="number">
+    <v-btn @click="showTransactionDetail">add</v-btn>
+    <transactionDetail v-if="transactionDetailFlag"></transactionDetail>
+  </div>
+</template>
+
+<script setup>
+import {ref} from 'vue'
+import transactionDetail from '@/components/transactionDetail.vue'
+const transactionDetailFlag = ref(false)
+const showTransactionDetail = () => {
+    transactionDetailFlag.value = !transactionDetailFlag.value
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/transactionDetail.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <form>
+        Name:<input type="text">
+        category:<input type="text">
+        bank account:<input type="text">
+        type: <input type="radio">
+        amount: <input type="text">
+        date: <input type="date">
+        note: <input type="text">
+        <v-btn>add</v-btn>
+    </form>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+
+</style>
