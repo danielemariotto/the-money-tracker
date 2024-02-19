0ccf8783affd0083c050c7ef06ddf1f86f4179a8
dan <daniele.mariotto@gmail.com>
2024-02-19 18:09:25 +0100

    Aggiunta vista giornaliera e funzionalità di inserimento transazione

---
+++ src/views/DailyView.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import insertTransaction from '@/components/insertTransaction.vue'
+const earning_list  = ref([{digits: 200, category: 'ecommerce'}, {digits: 300, category: 'investment'}])
+const expenses_list  = ref([{digits: 33, category: 'shopping'}, {digits: 45, category: 'shopping'}])
+const enterOperation = () => {
+    console.log('add entry')
+}
+</script>
+<template>
+    <div>
+        <v-container>
+            Daily
+            <v-row>
+              <v-col>
+              EARNING
+                    <div v-for="row, i in earning_list" :key="i">
+                        {{row.digits}} - {{row.category}}
+                    </div>
+              </v-col>
+              <v-col
+              >EXPENSES
+                    <div v-for="row, i in expenses_list" :key="i">
+                        {{row.digits}} - {{row.category}}
+                    </div>
+              </v-col>
+        </v-row>
+        <v-btn @click="enterOperation">add entry</v-btn>
+          </v-container>
+        <insertTransaction></insertTransaction>
+    </div>
+</template>
+<style>
+    
+</style>
