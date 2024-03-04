f8f603cf4772b4bb64b0847e5a18ab7b5b87409a
dan <daniele.mariotto@gmail.com>
2024-03-04 21:46:01 +0100

    daily view with mocked data

--- src/views/DailyView.vue
+++ src/views/DailyView.vue
@@ -1,35 +1,38 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-import insertTransaction from '@/components/insertTransaction.vue'
-const earning_list  = ref([{digits: 200, category: 'ecommerce'}, {digits: 300, category: 'investment'}])
-const expenses_list  = ref([{digits: 33, category: 'shopping'}, {digits: 45, category: 'shopping'}])
-const enterOperation = () => {
-    console.log('add entry')
-}
+import { ref } from "vue";
+import transactionDetail from "@/components/transactionDetail.vue";
+const transactions = ref([
+  { digits: 200, category: "ecommerce", type: "income" },
+  { digits: 300, category: "investment", type: "income" },
+  { digits: 400, category: "salary", type: "income" },
+  { digits: 33, category: "shopping", type: "expense" },
+  { digits: 45, category: "shopping", type: "expense" },
+]);
+
+const addNewTransaction = (data) => {
+  transactions.value.push(data);
+};
 </script>
 <template>
-    <div>
-        <v-container>
-            Daily
-            <v-row>
-              <v-col>
-              EARNING
-                    <div v-for="row, i in earning_list" :key="i">
-                        {{row.digits}} - {{row.category}}
-                    </div>
-              </v-col>
-              <v-col
-              >EXPENSES
-                    <div v-for="row, i in expenses_list" :key="i">
-                        {{row.digits}} - {{row.category}}
-                    </div>
-              </v-col>
-        </v-row>
-        <v-btn @click="enterOperation">add entry</v-btn>
-          </v-container>
-        <insertTransaction></insertTransaction>
-    </div>
+  <div>
+    <p class="text-center">Transactions</p>
+    <v-table>
+      <thead>
+        <tr>
+          <th class="text-left">Amount</th>
+          <th class="text-left">Category</th>
+          <th class="text-left">Type</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(t, i) in transactions" :key="i">
+          <td>{{ t.digits }}</td>
+          <td>{{ t.category }}</td>
+          <td>{{ t.type }}</td>
+        </tr>
+      </tbody>
+    </v-table>
+    <transactionDetail @new="addNewTransaction"></transactionDetail>
+  </div>
 </template>
-<style>
-    
-</style>
+<style></style>
